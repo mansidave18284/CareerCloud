da89a1a47cb66392494c46d23cf1806f04e207a7
mansidave18284 <mansidave18284@gmail.com>
2019-07-10 23:41:17 -0400

    First Commit

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,83 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    
+    public class ApplicantEducationLogic : BaseLogic<ApplicantEducationPoco>
+    {
+       // private const int saltLengthLimit = 10;
+
+        public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            //foreach (ApplicantEducationPoco poco in pocos)
+           // {
+                //poco.Password = ComputeHash(poco.Password, new byte[saltLengthLimit]);
+                //poco.Created = DateTime.Now.ToUniversalTime();
+                //poco.IsLocked = false;
+                //poco.IsInactive = false;
+                //poco.ForceChangePassword = true;
+                //poco.PasswordUpdate = poco.Created.AddDays(30);
+           // }
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+           // string[] requiredExtendedPasswordChars = new string[] { "$", "*", "#", "_", "@" };
+
+            foreach (var poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                {
+                    exceptions.Add(new ValidationException(107, $"Major for ApplicationEducation {poco.Id} cannot be empty"));
+                }
+                else if (poco.Major.Length <= 3)
+                {
+                    exceptions.Add(new ValidationException(107, $"Major for ApplicationEducation {poco.Id} must be at least 3 characters."));
+                }
+               
+                if (poco.StartDate != null)
+                {
+                    if(poco.StartDate > DateTime.Now)
+                    {
+                        exceptions.Add(new ValidationException(108, $"StartDate for ApplicationEducation {poco.Id} cannot be greater than today"));
+                    }                    
+                }
+
+                if (poco.CompletionDate != null)
+                {
+                    if (poco.CompletionDate < poco.StartDate)
+                    {
+                        exceptions.Add(new ValidationException(109, $"CompletionDate for ApplicationEducation {poco.Id} cannot be earlier than StartDate"));
+                    }
+                }
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+
+        
+    }
+}
---
+++ CareerCloud.Pocos/ApplicantWorkHistoryPoco.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Work_History")]
+    public class ApplicantWorkHistoryPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Applicant { get; set; }
+
+        [Column("Company_Name")]
+        public String CompanyName { get; set; }
+
+        [Column("Country_Code")]
+        public String CountryCode { get; set; }
+
+        public String Location { get; set; }
+
+        [Column("Job_Title")]
+        public String JobTitle { get; set; }
+
+        [Column("Job_Description")]
+        public String JobDescription { get; set; }
+
+        [Column("Start_Month")]
+        public short StartMonth { get; set; }
+
+        [Column("Start_Year")]
+        public int StartYear { get; set; }
+
+        [Column("End_Month")]
+        public short EndMonth { get; set; }
+
+        [Column("End_Year")]
+        public int EndYear { get; set; }
+
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
